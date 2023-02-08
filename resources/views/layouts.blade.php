131531bf2c2f8f1413c37b1dc233398f53ba8eb2
Yari <narducciyarigmail.com>
2023-02-08 16:38:51 +0100

    creazione route home

---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite('resources/scss/app.scss')
+    <title>Laravel Auth</title>
+</head>
+<body>
+    @include('header')
+</body>
+</html>
